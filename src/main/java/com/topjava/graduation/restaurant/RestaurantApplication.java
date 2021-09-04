3047ed9e4c81c6c5b0cb0c924edb031f71ff2a63
Dmitry Gidlevsky <dmitriy.gidlevskiy@gmail.com>
2021-09-04 21:13:51 +0300

    Removed useless bean

--- src/main/java/com/topjava/graduation/restaurant/RestaurantApplication.java
+++ src/main/java/com/topjava/graduation/restaurant/RestaurantApplication.java
@@ -40,10 +40,6 @@ public class RestaurantApplication {
         return hibernate5Module;
     }
 
-    @Primary
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
 
